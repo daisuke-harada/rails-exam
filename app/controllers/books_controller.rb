2ccc8267b7213db7ab7a9273c37624f4cce38576
vagrant <vagrant@localhost.localdomain>
2020-04-03 14:44:52 +0000

    [test-finish]rails-exam

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,8 @@ class BooksController < ApplicationController
       @book =Book.new(book_params)
       @books =Book.all
       if @book.save
-        redirect_to book_path(book.id)
-        flash[:notice] = 'Book was successfully created.'
+         redirect_to book_path(@book.id)
+         flash[:notice] = 'Book was successfully created.'
       else
         render action: :index
       end
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
   def update
       @book =Book.find(params[:id])
       if @book.update(book_params)
-         redirect_to book_path(book.id)
+         redirect_to book_path(@book.id)
          flash[:update] = 'Book was successfully updated.'
       else
         render action: :edit
